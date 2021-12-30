2031ceb047d93397c341b4e5026e3db2239e84f9
federicobaldini <federico-baldini@hotmail.com>
2021-12-30 16:16:17 +0100

    added style to UserItem component

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import Avatar from "../../shared/components/UI/Avatar";
+import Card from "../../shared/components/UI/Card";
 import "./UserItem.css";
 
 const UserItem = (props) => {
-  return <li className="user-item">
-    <div className="user-item__content">
-      <div className="user-item__image">
-        <img src={props.image} alt={props.name} />
-      </div>
-      <div className="user-item__info">
-        <h2>{props.name}</h2>
-        <h3>{props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}</h3>
-      </div>
-    </div>
-  </li>;
+  return (
+    <li className="user-item">
+      <Card className="user-item__content">
+        <Link to={"/" + props.id + "/places"}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
+    </li>
+  );
 };
 
 export default UserItem;
