af468ba1ce6c71c8b035b931189f8f9607fda12d
federicobaldini <federico-baldini@hotmail.com>
2021-12-30 15:27:44 +0100

    added new UsersList component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>
+  const USERS = [
+    {
+      id: "u00001",
+      name: "Federico Baldini",
+      image:
+        "https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg",
+      places: 7,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
