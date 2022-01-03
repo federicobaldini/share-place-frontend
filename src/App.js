75324a8e6bb5cdaccbbe9ad7d1e8057bcddcde96
federicobaldini <federico-baldini@hotmail.com>
2022-01-03 11:31:57 +0100

    added new UserPlaces component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 const App = () => {
   return (
@@ -18,6 +19,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const UserPlaces = () => {
+  const PLACES = [
+    {
+      id: "p00001",
+      title: "Empire State Building",
+      description: "One of the must famous sky scrapers in the world!",
+      imageUrl:
+        "https://static.fanpage.it/wp-content/uploads/sites/10/2021/04/empire-state-building-1081929_1920-1200x675.jpg",
+      address: "20 W 34th St, New York, NY 10001",
+      location: {
+        lat: 40.7484405,
+        lng: -73.9878584,
+      },
+      creator: "u00001",
+    },
+    {
+      id: "p00002",
+      title: "Empire State Building",
+      description: "One of the must famous sky scrapers in the world!",
+      imageUrl:
+        "https://static.fanpage.it/wp-content/uploads/sites/10/2021/04/empire-state-building-1081929_1920-1200x675.jpg",
+      address: "20 W 34th St, New York, NY 10001",
+      location: {
+        lat: 40.7484405,
+        lng: -73.9878584,
+      },
+      creator: "u00002",
+    },
+  ];
+
+  const userId = useParams().userId;
+  const loadedPlaces = PLACES.filter((place) => place.creator === userId);
+
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
