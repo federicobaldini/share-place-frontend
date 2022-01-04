b376ec16b9c9e48259217e3ad41210a036341782
federicobaldini <federico-baldini@hotmail.com>
2022-01-04 16:06:56 +0100

    added new Input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Input from "../../shared/components/Form/Input";
+import "./NewPlace.css";
+
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>;
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 };
 
 export default NewPlace;
