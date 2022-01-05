9d6cc50ec57d47e5c476a80c8164e2c4ffaa465f
federicobaldini <federico-baldini@hotmail.com>
2022-01-05 15:50:17 +0100

    managed state in the Input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,7 @@ import "./NewPlace.css";
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input element="input" type="text" label="Title" validators={[]} errorText="Please enter a valid title." />
     </form>
   );
 };
