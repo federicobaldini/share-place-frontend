d5df185e5902418d9d1f42e5a51bc5b647e18531
federicobaldini <federico-baldini@hotmail.com>
2022-01-07 22:19:00 +0100

    fixed minor issues

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../../shared/components/UI/Card";
+import Button from "../../shared/components/Form/Button";
 import PlaceItem from "./PlaceItem";
 import "./PlaceList.css";
 
@@ -10,7 +11,7 @@ const PlaceList = (props) => {
       <div className="place-list center">
         <Card>
           <h2>No places found. Maybe create one?</h2>
-          <button>Share Place</button>
+          <Button to="/places/new">Share Place</Button>
         </Card>
       </div>
     );
--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 import Input from "../../shared/components/Form/Input";
 import Button from "../../shared/components/Form/Button";
+import Card from "../../shared/components/UI/Card";
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
@@ -60,19 +61,21 @@ const UpdatePlace = () => {
   const identifiedPlace = PLACES.find((p) => p.id === placeId);
 
   useEffect(() => {
-    setFormData(
-      {
-        title: {
-          value: identifiedPlace.title,
-          isValid: true,
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true,
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true,
+          },
         },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true,
-        },
-      },
-      true
-    );
+        true
+      );
+    }
     setIsLoading(false);
   }, [setFormData, identifiedPlace]);
 
@@ -84,7 +87,9 @@ const UpdatePlace = () => {
   if (!identifiedPlace) {
     return (
       <div className="center">
-        <h2>Could not find place</h2>
+        <Card>
+          <h2>Could not find place</h2>
+        </Card>
       </div>
     );
   }
@@ -92,7 +97,9 @@ const UpdatePlace = () => {
   if (isLoading) {
     return (
       <div className="center">
-        <h2>Loading...</h2>
+        <Card>
+          <h2>Loading...</h2>
+        </Card>
       </div>
     );
   }
