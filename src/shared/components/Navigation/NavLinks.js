d01a49a43edf80716d5eebdd693fa3ed4fa1ddb1
federicobaldini <federico-baldini@hotmail.com>
2022-01-11 19:59:48 +0100

    added useContext to NavLinks component

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
+import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
@@ -11,15 +14,21 @@ const NavLinks = (props) => {
           ALL USERS
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u00001/places">MY PLACES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">ADD PLACE</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u00001/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
