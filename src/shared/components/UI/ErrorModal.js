c147b083c5762b06a914bca7dbf0d13c176be70f
federicobaldini <federico-baldini@hotmail.com>
2022-01-29 11:51:12 +0100

    added new ErrorModal component

---
+++ src/shared/components/UI/ErrorModal.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Modal from "./Modal";
+import Button from "../Form/Button";
+import "./ErrorModal.css";
+
+const ErrorModal = (props) => {
+  return (
+    <Modal
+      onCancel={props.onClear}
+      header="An Error Occurred!"
+      show={!!props.error}
+      footer={<Button onClick={props.onClear}>Okay</Button>}
+    >
+      <p>{props.error}</p>
+    </Modal>
+  );
+};
+
+export default ErrorModal;
