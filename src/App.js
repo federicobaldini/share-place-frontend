f82e6bdd3b450a6cfb91d84762042f9a7215d089
federicobaldini <federico-baldini@hotmail.com>
2022-02-14 18:52:49 +0100

    added auto-login using Local Storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Users from "./user/pages/Users";
@@ -16,12 +16,24 @@ const App = () => {
   const login = useCallback((userId, token) => {
     setToken(token);
     setUserId(userId);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({ userId: userId, token: token })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   }, []);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (storedData && storedData.token) {
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
 
   let routes;
 
