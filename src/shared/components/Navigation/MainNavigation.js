b8c05dfd2775064d27e564bedbe59862c4bb30f4
federicobaldini <federico-baldini@hotmail.com>
2022-03-06 12:39:51 +0100

    changed application style

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -36,7 +36,7 @@ const MainNavigation = (props) => {
           <span></span>
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">Your Places</Link>
+          <Link to="/">Share a Place!</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
